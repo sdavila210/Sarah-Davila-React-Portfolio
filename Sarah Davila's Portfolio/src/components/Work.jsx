0db2bfb5e093eeb026fa7713d9ca09c3b56fcbee
sarahd <sarahjdavila210@gmail.com>
2024-05-16 23:13:57 -0700

    created the basic outline for portfolio. Created code for App.jsx and started working on components. Also made corresponding CSS to go with the html.

---
+++ Sarah Davila's Portfolio/src/components/Work.jsx
@@ -0,0 +1,10 @@
+function Work() {
+    return (
+        <section id="work" className="about article">
+            <h2>Portfolio</h2>
+            { }
+        </section>
+    );
+}
+
+export default Work;
